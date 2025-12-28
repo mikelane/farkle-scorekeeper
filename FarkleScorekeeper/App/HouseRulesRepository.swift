c3cce750bc8832f6f9de7a0bcf0124d629a9eaad
Mike Lane <mikelane@gmail.com>
2025-12-28 10:53:47 -0800

    feat: Add HouseRulesRepository for UserDefaults persistence
    Add HouseRulesRepository class with:
    - load(): retrieves saved rules or returns defaults
    - save(_:): persists rules to UserDefaults as JSON
    - reset(): clears stored rules
    - Dependency injection for UserDefaults (testability)
    - Static storageKey constant
    
    Part of #34

---
+++ FarkleScorekeeper/App/HouseRulesRepository.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+final class HouseRulesRepository: Sendable {
+    static let storageKey = "houseRules"
+
+    private let userDefaults: UserDefaults
+
+    init(userDefaults: UserDefaults = .standard) {
+        self.userDefaults = userDefaults
+    }
+
+    func load() -> HouseRules {
+        guard let data = userDefaults.data(forKey: Self.storageKey),
+              let rules = try? JSONDecoder().decode(HouseRules.self, from: data)
+        else {
+            return HouseRules()
+        }
+        return rules
+    }
+
+    func save(_ rules: HouseRules) {
+        guard let data = try? JSONEncoder().encode(rules) else {
+            return
+        }
+        userDefaults.set(data, forKey: Self.storageKey)
+    }
+
+    func reset() {
+        userDefaults.removeObject(forKey: Self.storageKey)
+    }
+}
